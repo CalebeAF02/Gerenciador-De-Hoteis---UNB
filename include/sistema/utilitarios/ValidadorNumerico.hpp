066732eb417c6953e06bc330c8871136a96ce7e7
CalebeAF02 <calebe.2324@gmail.com>
2025-11-09 11:19:07 -0300

    feature: Verifica se a string é um número inteiro

---
+++ include/sistema/utilitarios/ValidadorNumerico.hpp
@@ -0,0 +1,18 @@
+//
+// Created by caleb on 09/11/2025.
+//
+
+#ifndef GERENCIADOR_DE_HOTEIS_UNB_VALIDADORNUMERICO_HPP
+#define GERENCIADOR_DE_HOTEIS_UNB_VALIDADORNUMERICO_HPP
+
+#include <string>
+
+using namespace std;
+
+class ValidadorNumerico {
+public:
+    static bool verificaSeENumero(const string& str);
+};
+
+
+#endif //GERENCIADOR_DE_HOTEIS_UNB_VALIDADORNUMERICO_HPP
---
+++ src/sistema/utilitarios/ValidadorNumerico.cpp
@@ -0,0 +1,20 @@
+//
+// Created by caleb on 09/11/2025.
+//
+
+#include "../../../include/sistema/utilitarios/ValidadorNumerico.hpp"
+
+bool ValidadorNumerico::verificaSeENumero(const string& str) {
+
+    // passa por cada caracter, e verifica se a string e um numero
+
+    if (str.empty()) {
+        return false;
+    }
+    for (char c : str) {
+        if (!isdigit(static_cast<unsigned char>(c))) {
+            return false;
+        }
+    }
+    return true;
+}
